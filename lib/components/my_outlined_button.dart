6aa27c1dcc993519f21e950ba577b804d2f3c7b0
MohammadAminBahari <bahariamin.1384@gmail.com>
2024-09-03 15:33:57 +0330

    finished OutlinedButton component

---
+++ lib/components/my_outlined_button.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class MyOutlinedButton extends StatelessWidget {
+  const MyOutlinedButton({
+    super.key,
+    required this.hasIcon,
+    required this.text,
+    required this.color,
+    required this.onTap,
+    this.icon,
+    this.isIconRight,
+  });
+  final bool hasIcon;
+  final String text;
+  final Color color;
+  final VoidCallback onTap;
+  final IconData? icon;
+  final bool? isIconRight;
+
+  @override
+  Widget build(BuildContext context) {
+    if (hasIcon) {
+      if (isIconRight!) {
+        return GestureDetector(
+          onTap: onTap,
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: color,
+                  width: 3.0,
+                ),
+                borderRadius: BorderRadius.circular(20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon,
+                  size: 30,
+                  color: color,
+                ),
+                Text(
+                  "$text  ",
+                  style: TextStyle(
+                    color: color,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: color,
+                width: 3.0,
+              ),
+              borderRadius: BorderRadius.circular(20)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "  $text",
+                style: TextStyle(
+                  color: color,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(
+                icon,
+                size: 100,
+                color: color,
+              )
+            ],
+          ),
+        );
+      }
+    } else {
+      return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: color,
+                width: 3.0,
+              ),
+              borderRadius: BorderRadius.circular(20)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$text",
+                style: TextStyle(
+                  color: color,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
